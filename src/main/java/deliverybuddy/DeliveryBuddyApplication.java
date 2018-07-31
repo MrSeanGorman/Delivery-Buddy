d928409cf975cd4f4ea6ed54f3368023c026f710
MrSeanGorman <mrseangorman@gmail.com>
2018-07-30 21:58:07 -0500

    First commit

---
+++ src/main/java/deliverybuddy/DeliveryBuddyApplication.java
@@ -0,0 +1,12 @@
+package deliverybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryBuddyApplication.class, args);
+	}
+}
---
+++ src/test/java/deliverybuddy/DeliveryBuddyApplicationTests.java
@@ -0,0 +1,16 @@
+package deliverybuddy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeliveryBuddyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
